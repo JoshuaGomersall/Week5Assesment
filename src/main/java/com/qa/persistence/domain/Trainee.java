5d4824abf61601be7909cd92b3553154b449da97
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-02-15 11:42:55 +0000

    Add files via upload

--- src/main/java/com/qa/persistence/domain/Trainee.java
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -1,33 +1,39 @@
 package com.qa.persistence.domain;
 
-public class Trainee 
-{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trainee {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private long traineeId = 1l;
 	private String traineeName = "";
-	
-	
-	
-	
-	
-	
-	
+
 	public long getTraineeId() {
 		return traineeId;
 	}
+
 	public void setTraineeId(long traineeId) {
 		this.traineeId = traineeId;
 	}
+
 	public String getTraineeName() {
 		return traineeName;
 	}
+
 	public void setTraineeName(String traineeName) {
 		this.traineeName = traineeName;
 	}
+
 	public Trainee(long traineeId, String traineeName) {
 		super();
 		this.traineeId = traineeId;
 		this.traineeName = traineeName;
 	}
+
 	public Trainee() {
-	}	
+	}
 }
--- src/main/java/com/qa/persistence/domain/Trainer.java
+++ src/main/java/com/qa/persistence/domain/Trainer.java
@@ -1,37 +1,59 @@
 package com.qa.persistence.domain;
 
-public class Trainer 
-{
-	private Long classroomid = 1l; 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Trainer {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long classroomid = 1l;
 	private String trainerName = "";
-	
-	
-	
-	public Trainer()
-	{
-		
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Trainee> trainees = new ArrayList<>();
+
+	public void addtrainee(Trainee trainee) {
+		trainees.add(trainee);
 	}
-	
+
+	public void removetrainee(Trainee trainee) {
+		trainees.remove(trainee);
+	}
+
+	// Constructors, getters and setters removed for brevity
+
+	public Trainer() {
+
+	}
+
 	public Trainer(Long classroomid, String trainerName) {
 		super();
 		this.classroomid = classroomid;
 		this.trainerName = trainerName;
 	}
-	
-	
+
 	public Long getClassroomid() {
 		return classroomid;
 	}
+
 	public void setClassroomid(Long classroomid) {
 		this.classroomid = classroomid;
 	}
+
 	public String getTrainerName() {
 		return trainerName;
 	}
+
 	public void setTrainerName(String trainerName) {
 		this.trainerName = trainerName;
 	}
-	
-	
-	
+
 }
